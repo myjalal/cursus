9aa84ccfdf59145ab89281872561b17299f6287e
Jalal <jecherkaoui@student.42quebec.com>
2021-10-03 16:01:19 -0400

    last update of trip 03 oct 2021

---
+++ lib_ft/ft_strnstr.c
@@ -0,0 +1,54 @@
+#include <string.h>
+#include <xlocale.h>
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strnstr(const char *haystack, const char *needle, unsigned int len)
+{
+	int		i;
+	int		j;
+	int		p;
+
+	i = 0;
+	j = 0;
+	if (*needle == '\0')
+		return ((char *)haystack);
+	while (haystack[i] && i < len)
+	{
+		if (haystack[i] == needle[j])
+		{
+			p = i;
+			while (haystack[i] == needle[j])
+			{
+				i++;
+				j++;
+			}
+		}
+		if (j == ft_strlen((char *)needle))
+			return ((char *)haystack + p);
+		i++;
+		j = 0;
+	}
+	return (NULL);
+}
+
+/*int main(void)
+{
+	const char *largestring = "Foo Bar Baz";
+	const char *smallstring = "B";
+	char *ptr;
+	char *ft_ptr;
+	 
+	ptr = strnstr(largestring, smallstring, 11);
+	ft_ptr = ft_strnstr(largestring, smallstring, 11);
+	printf("real: %p and mine is: %p\n", ptr, ft_ptr);
+}*/
