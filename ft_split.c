3e41537e73ee0175c49f79919d85520cc1e20daf
EC2 Default User <ec2-user@ip-172-31-39-75.us-east-2.compute.internal>
2021-10-22 14:00:49 +0000

    added a test for ft_split

---
+++ ft_split.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char string[] = "I,Love,pumpkin";
+	char *token = strtok(string, ",";
+	char *nina = ft_split(string, ",";
+	
+	printf("this is strtok: %s\nthis is ft_split: %s\n", token, nina);	
+
+}
+
