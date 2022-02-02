8a70fd852340cb078d0db85d959e664e79d966b1
Jalal <echerkaouijalal@gmail.com>
2022-02-01 20:33:44 -0500

    update

---
+++ lib_ft/ft_itoa.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+char *ft_writenum(int n)
+{
+	
+}
+
+int ft_size(int n)
+{
+	
+}
+
+char *ft_itoa(int n)
+{
+	
+}
+
+int main(void)
+{
+	int i;
+	
+	i = 0;
+	
+}
---
+++ lib_ft/ft_strlen.c
@@ -0,0 +1,10 @@
+#include <stddef.h>
+size_t ft_strlen(const char *c)
+{
+	int i;
+	i = 0;
+	
+	while (c[i])
+		i++;
+	return (i);
+}
