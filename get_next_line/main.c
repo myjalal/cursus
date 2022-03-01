50a586980b6c62c047b6e385794b8a5828a1b265
Jalal eddine Echekaoui <jechekao@c2r6p5.42quebec.com>
2022-03-01 15:14:52 -0500

    update mardi 1 avril

---
+++ get_next_line/main.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jechekao <jechekao@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/12 09:14:39 by jechekao         #+#    #+#             */
+/*   Updated: 2021/11/28 14:19:47 by jechekao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	int		fd_1;
+	int		i;
+	char	*line[4096];
+
+	i = 1;
+	fd_1 = open("text", O_RDONLY);
+	while (1)
+	{
+		line[fd_1] = get_next_line(fd_1);
+		printf("Line %d for fd %d: %s\n", i, fd_1, line[fd_1]);
+		if (!line[fd_1])
+			return (0);
+		free(line[fd_1]);
+		i++;
+	}
+}
