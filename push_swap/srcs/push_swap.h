f4a92588e24f02efdce3aa2e70e2f72daf3d3897
Jalal eddine Echekaoui <jechekao@c1r3p8.42quebec.com>
2022-07-03 11:30:31 -0400

    updated on Dim  3 jul 2022 11:30:31 EDT

--- push_swap/srcs/push_swap.h
+++ push_swap/srcs/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jechekao <jechekao@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 19:34:33 by jechekao          #+#    #+#             */
-/*   Updated: 2022/07/02 18:26:12 by jechekao         ###   ########.fr       */
+/*   Updated: 2022/07/03 11:29:47 by jechekao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,12 +62,16 @@ int					sort_checker(t_node **list, int len);
 /*--------populate.c--------*/
 // NULL list for initiation
 t_var				*list_init(t_var *list);
+
 // Temp argv list,  list, temp node
 t_node				*list_filler(char **argv, t_var *list, t_node *temp);
+
 // Check if there is a top node, then either creats one or adds a node
 void				add_node(t_node **top, t_node *node);
+
 // Add number to top of the list
 void 				top(t_node **list, t_node *node);
+
 // Add number to bottom of the list
 void 				bottom(t_node **list, t_node *node);
 
@@ -80,9 +84,26 @@ int					error_exit(char *error, int err_exit);
 // Free linked list
 void				ft_free(t_node **list);
 
+/*---------move.c-----------*/
+// oparations
+
+// SA and SB
+void	swap(t_node **list);
+
+//   PA and PB
+void	one_top_two(t_node **l_one, t_node **l_two);
+
+// RA and RB
+void	revers(t_node **list);
+
+// RRA and RRB
+void	r_revers(t_node **list);
+
 /*---------test_tools.c------*/ // !!!!! DELETE
 // Print linked list to test
 void				print_list(t_node *list);
+
+
 
 #endif
 
